ae409cc277c0c9c86adffe46e46ee1182eca87e5
coolfalafel46 <coolfalafel46@gmail.com>
2025-06-23 15:02:39 +0300

    modified wall.cpp

--- wall/wall.cpp
+++ wall/wall.cpp
@@ -1,5 +1,3 @@
-// wall.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <fstream>
@@ -13,12 +11,12 @@ using json = nlohmann::json;
 int main()
 {
     string input;
-    //json tempdata = json::object();
+
     vector<string> keys;
     string penis;
     cout << "(c) Brickwall - 2025 DKM Community\n";
-    //getline(cin,input);
-    input = R"(C:\Users\superiorFexin\Desktop\testing.json)";
+    getline(cin,input);
+
     cout << input + "\n";
 
 
@@ -29,11 +27,7 @@ int main()
 
     for (auto& tempdata : data.items()) {
     
-        //cout << tempdata.key() << "\n";
-        /*cout << tempdata.value().dump() << "\n"; */
-        //cout << tempdata.value() << "\n";
 
-        //aaa blyat koroche suka shtuki
         for (auto& j : tempdata.value().items()) {
         
             if (jitems.find(j.key()) == jitems.end()) {
@@ -49,8 +43,7 @@ int main()
     
     for (auto it = jitems.begin(); it != jitems.end(); ++it)
     {
-        // Add the key to the vector
-        //keys.push_back(it->first);
+
         cout << it->first << "(" << jitems[it->first] <<")" << "\n";
         
     }
@@ -65,22 +58,6 @@ int main()
         }
     }
 
-    /*for (int i = 0; i < data.size(); i++) {
-    
-        cout << data[i] << "\n";
-
-    }*/
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
